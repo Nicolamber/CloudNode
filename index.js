f925ef1fa71447ee1f7e47ce0b0db660364a1e01
NahuelValencia-eb <nahuel.valencia@eventbrite.com>
2019-09-27 19:11:09 -0300

    lambda functions added

--- index.js
+++ index.js
@@ -1,57 +1,124 @@
-var AWS = require('aws-sdk');
-var handler = async (​ event​ ) => {
-var dynamodb = new AWS.DynamoDB({
-apiVersion: '2012-08-10',
-endpoint: 'http://​dynamodb​:8000',
-region: 'us-west-2',
-credentials: {
-accessKeyId: '2345',
-secretAccessKey: '2345'
-}
+const AWS = require("aws-sdk");
+
+const dynamodb = new AWS.DynamoDB({
+  apiVersion: "2012-08-10",
+  endpoint: "http://dynamodb:8000",
+  region: "us-west-2",
+  credentials: {
+    accessKeyId: "2456",
+    secretAccessKey: "2456"
+  }
 });
-var docClient = new AWS.DynamoDB.DocumentClient({
-apiVersion: '2012-08-10',
-service: dynamodb
+const docClient = new AWS.DynamoDB.DocumentClient({
+  apiVersion: "2012-08-10",
+  service: dynamodb
 });
+
+const getEnviosPendientes = async () => {
+  try {
+    var params = {
+      TableName: "Envio",
+      IndexName: "idPendiente",
+      Limit: 20
+    };
+    return new Promise((resolve, reject) => {
+      docClient.scan(params, (err, data) => {
+        if (err) reject(err);
+        else resolve(data);
+      });
+    });
+  } catch (e) {
+    console.log("TCL: getEnviosPendientes -> e", e);
+  }
+};
+
+const createEnvio = async body => {
+  try {
+    var params = {
+      TableName: "Envio",
+      Item: {
+        // a map of attribute name to AttributeValue
+        id: Date.now().toString(),
+        fechaAlta: new Date().toJSON(),
+        destino: body.destino,
+        email: body.email,
+        pendiente: "si"
+      }
+    };
+    return new Promise((resolve, reject) => {
+      docClient.put(params, (err, data) => {
+        if (err) reject(err);
+        else resolve("Se ha creado con exito!");
+      });
+    });
+  } catch (e) {
+    console.log("TCL: e", e);
+  }
+};
 
 const getEnvio = async id => {
-    try {
-      var params = {
-        TableName: "envio",
-        
-        KeyConditionExpression: "id = :var", 
-        ExpressionAttributeValues: {
-          ":var": id
-        }
-      };
-      return new Promise((resolve, reject) => {
-        docClient.query(params, (err, data) => {
-          if (err) reject(err);
-          else resolve(data);
-        });
+  try {
+    var params = {
+      TableName: "Envio",
+      //IndexName: 'id', // optional (if querying an index)
+      KeyConditionExpression: "id = :var", // a string representing a constraint on the attribute
+      ExpressionAttributeValues: {
+        // a map of substitutions for all attribute values
+        ":var": id
+      }
+    };
+    return new Promise((resolve, reject) => {
+      docClient.query(params, (err, data) => {
+        if (err) reject(err);
+        else resolve(data);
       });
-    } catch (e) {
-      console.log("Error al traer los envios: ", e);
-    }
-  };
-
-const getEnvioPendiente = async () => {
-    try{ 
-        var params = {
-            TableName: "envio",
-            IndexName: "envios_pendientes_index",
-            Limit: 30
-        };
-        return new Promise((resolve,reject) =>{
-            docClient.scan(params,(err,data)=>{
-                if(err) reject (err);
-                else resolve(data);
-            });
-        });
-    }catch(e){
-        console.log("Error al traer los envios pendientes: ", e);
-    }
+    });
+  } catch (e) {
+    console.log("TCL: getEnviosPendientes -> e", e);
+  }
 };
 
+const setEnvioEntregado = async id => {
+  const idParsed = id.toString()
+  try {
+    var params = {
+      TableName: "Envio",
+      Key: { id: idParsed },
+      UpdateExpression: "remove pendiente"
+    };
+    return new Promise((resolve, reject) => {
+      docClient.update(params, (err, data) => {
+        if (err) reject(err);
+        else resolve(data);
+      });
+    });
+  } catch (e) {
+    console.log("TCL: getEnviosPendientes -> e", e);
+  }
 };
-exports.handler = handler;
+
+exports.handler = async event => {
+  try {
+    const id = (event.pathParameters || {}).id || false;
+    const body = JSON.parse(event.body);
+
+    switch (event.resource) {
+      case "/envios":
+        return { body: JSON.stringify(await createEnvio(body)) };
+        break;
+      case "/envios/pendientes":
+        return { body: JSON.stringify(await getEnviosPendientes()) };
+      case "/envios/{id}":
+        return { body: JSON.stringify(await getEnvio(id)) };
+        break;
+      case "/envios/{id}/entregado":
+        return { body: JSON.stringify(await setEnvioEntregado(id)) };
+        break;
+
+      default:
+        break;
+    }
+  } catch (e) {
+    console.log("TCL: e", e);
+  }
+};
